3dd67e5241766a61993aee06f8ccc43ddec905c2
boudiaj <boudiajhiba2@gmail.com>
2026-07-02 22:24:52 +0100

    api work Done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,32 +1,25 @@
+import { useEffect } from "react";
 import Hero from "../../components/Hero/Hero";
-// import AnimeSection from "../../components/AnimeSection/AnimeSection";
-
-// import { HiTrendingUp } from "react-icons/hi";
-// import { LuCalendarDays } from "react-icons/lu";
-
+import { getTrendingAnime, getSeasonNow } from "../../services/animeService";
 
 function Home() {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const trending = await getTrendingAnime();
+        const seasonal = await getSeasonNow();
+
+        console.log("Trending:", trending);
+        console.log("Seasonal:", seasonal);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <>
       <Hero />
-{/* 
-      <AnimeSection
-        title="Trending Anime"
-        subtitle="The most popular series in the global community"
-        icon={<HiTrendingUp />}
-        buttonText="VIEW ALL"
-        buttonLink="/anime"
-        // animeList={trendingAnime}
-      />
-
-      <AnimeSection
-      title="Seasonal Anime"
-      subtitle="The hottest series airing in the current season"
-      icon={<LuCalendarDays />}
-      buttonText="VIEW SEASON"
-      buttonLink="/anime"
-      // animeList={seasonalAnime}
-      /> */}
     </>
   );
 }
