873b4bee8539bb2cac01fdcac279bd62093d6f29
Hibaboudiaj <boudiajhiba2@gmail.com>
2026-07-02 10:37:26 +0100

    fixing Section SeasonalAnime

---
+++ src/data/seasonalAnime.js
@@ -0,0 +1,65 @@
+import frieren from "../assets/frieren.jpg";
+import rezero from "../assets/rezero.jpg";
+import jojo from "../assets/jojo.jpg";
+import fmab from "../assets/fmab.jpg";
+import steins from "../assets/steins.jpg";
+import chainsaw from "../assets/chainsaw.jpg";
+
+const seasonalAnime = [
+  {
+    id: 1,
+    title: "Solo Leveling Season 2",
+    image: chainsaw,
+    type: "TV",
+    score: 8.9,
+    episodes: 13,
+    year: 2025,
+  },
+  {
+    id: 2,
+    title: "Wind Breaker Season 2",
+    image: steins,
+    type: "TV",
+    score: 8.4,
+    episodes: 12,
+    year: 2025,
+  },
+  {
+    id: 3,
+    title: "Witch Watch",
+    image: fmab,
+    type: "TV",
+    score: 7.9,
+    episodes: 24,
+    year: 2025,
+  },
+  {
+    id: 4,
+    title: "Lazarus",
+    image: frieren,
+    type: "TV",
+    score: 8.1,
+    episodes: 13,
+    year: 2025,
+  },
+  {
+    id: 5,
+    title: "Kaiju No. 8 Season 2",
+    image: rezero,
+    type: "TV",
+    score: 8.6,
+    episodes: 12,
+    year: 2025,
+  },
+  {
+    id: 6,
+    title: "Fire Force Season 3",
+    image: jojo,
+    type: "TV",
+    score: 8.3,
+    episodes: 24,
+    year: 2025,
+  },
+];
+
+export default seasonalAnime;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,21 +2,32 @@ import Hero from "../components/Hero/Hero";
 import AnimeSection from "../components/AnimeSection/AnimeSection";
 
 import { HiTrendingUp } from "react-icons/hi";
+import { LuCalendarDays } from "react-icons/lu";
 
 import trendingAnime from "../data/trendingAnime";
+import seasonalAnime from "../data/seasonalAnime";
 
 function Home() {
   return (
     <>
       <Hero />
 
-      <AnimeSection 
-      title="Trending Anime"
-      subtitle="The most popular series in the global community"
-      icon={<HiTrendingUp />}
-      buttonText="VIEW ALL"
+      <AnimeSection
+        title="Trending Anime"
+        subtitle="The most popular series in the global community"
+        icon={<HiTrendingUp />}
+        buttonText="VIEW ALL"
+        buttonLink="/anime"
+        animeList={trendingAnime}
+      />
+
+      <AnimeSection
+      title="Seasonal Anime"
+      subtitle="The hottest series airing in the current season"
+      icon={<LuCalendarDays />}
+      buttonText="VIEW SEASON"
       buttonLink="/anime"
-      animeList={trendingAnime}
+      animeList={seasonalAnime}
       />
     </>
   );
