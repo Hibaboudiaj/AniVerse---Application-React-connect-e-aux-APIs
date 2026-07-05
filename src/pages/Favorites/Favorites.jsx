60328a63cfe61c6cb91031fe0716442735c024b6
boudiaj <boudiajhiba2@gmail.com>
2026-07-05 19:19:12 +0100

    create favorites page header

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,4 +1,38 @@
+import { FaHeart } from "react-icons/fa";
+
+import useFavorite from "../../hooks/useFavorite";
+
+import styles from "./Favorites.module.css";
+
 function Favorites() {
-  return <h1>Anime Page</h1>;
+  const { favorites } = useFavorite();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <div className={styles.left}>
+          <div className={styles.icon}>
+            <FaHeart />
+          </div>
+
+          <div>
+            <h1>My Favorites</h1>
+
+            <p>
+              A persistent collection of your absolute favorite anime
+              series.
+            </p>
+          </div>
+        </div>
+
+        <div className={styles.total}>
+          <span>TOTAL SAVED :</span>
+
+          <strong>{favorites.length}</strong>
+        </div>
+      </div>
+    </div>
+  );
 }
+
 export default Favorites;
