6e24fa9b164e8614fe48165869fb88c9246b8ce5
boudiaj <boudiajhiba2@gmail.com>
2026-07-05 18:29:37 +0100

    db.json axios jsonServer favoritService

---
+++ src/services/favoriteService.js
@@ -0,0 +1,16 @@
+import jsonServer from "../api/jsonServer";
+
+// get all
+export async function getFavorites() {
+  const response = await jsonServer.get("/favorites");
+  return response.data;
+}
+
+export async function addFavorite(anime) {
+  const response = await jsonServer.post("/favorites", anime);
+  return response.data;
+}
+
+export async function removeFavorites(id) {
+  await jsonServer.delete(`/favorites/${id}`);
+}
